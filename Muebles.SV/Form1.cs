5b38e659bd27babf597181531c2a153c41693428
Karla Reneé Rivas Hernández <20200308@ricaldone.edu.sv>
2022-05-25 13:19:23 -0600

    primer comando

--- Muebles.SV/Form1.cs
+++ Muebles.SV/Form1.cs
@@ -15,7 +15,30 @@ namespace Muebles.SV
         public Form1()
         {
             InitializeComponent();
+            
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox1.Text == "Usuario")
+            {
+                textBox1.Text = " ";
+            }
+        }
     }
 }
